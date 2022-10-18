3a8aaabd39de8181d7f3e336af1def38cb19c0b1
MacAnderson Uche <macanderson.uche@productcaregroup.com>
2022-10-19 00:14:43 +0100

    removed dynamodb

--- Repo 4/src/utils/getAllUsers/index.ts
+++ Repo 4/src/utils/getAllUsers/index.ts
@@ -3,15 +3,9 @@ import userGenerator from "../../database/db1";
 // import { read } from "../../filesystem";
 import { User } from "../../types";
 
-// export default function getAllUsers(): User[] {
-//     // let dataToReturn: User[];
-//     // const data =  read("src/database/db.json");
-// 	// return dataToReturn = JSON.parse(data);
-//     return usersArray;
-// }
-export default async function getAllUsers() {
-    const userArr = await userGenerator();
-    if(userArr) {
-        return userArr.users
-    }
+export default function getAllUsers(): User[] {
+    // let dataToReturn: User[];
+    // const data =  read("src/database/db.json");
+	// return dataToReturn = JSON.parse(data);
+    return usersArray;
 }
